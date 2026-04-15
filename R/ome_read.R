0b281ed953f18ff8a1fa99045e4ffc23ff27c661
Hugo Gruson <git@hugogruson.fr>
2026-04-15 09:15:27 +0200

    Add docs to ome_read()

--- R/ome_read.R
+++ R/ome_read.R
@@ -1,4 +1,14 @@
-#' @examples 
+#' Read a multiscale OME-Zarr file
+#' 
+#' @param path Path to the OME-Zarr file.
+#' @inheritParams Rarr::read_zarr_array
+#' @param lazy Logical. If `TRUE` (the default), use \pkg{ZarrArray}
+#'   to read data lazily. If `FALSE`, read data into memory using 
+#'   \pkg{Rarr}. If the data can fit into memory, setting `lazy = FALSE`
+#'   may result in better performance.
+#' @param validate Logical.If `TRUE` (the default), validate the OME-Zarr file.
+#' 
+#' @examples
 #' ome_read(
 #'   system.file("extdata", "ome-v0.2", "6001247.zarr", package = "rome")
 #' )
@@ -11,7 +21,7 @@
 #'   system.file("extdata", "ome-v0.4", "10501752.zarr", package = "rome")
 #' )
 
-ome_read <- function(path, s3_client = NULL, validate = TRUE) {
+ome_read <- function(path, s3_client = NULL, lazy = TRUE, validate = TRUE) {
 
   # FIXME: check we're in a group
   if (validate) {

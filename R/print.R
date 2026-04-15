dea67992c9353acf93d248c3a043c957fefde226
Hugo Gruson <git@hugogruson.fr>
2026-04-15 14:30:24 +0200

    Add missing newline in print method

--- R/print.R
+++ R/print.R
@@ -2,6 +2,6 @@
 #' @export
 print.ome_zarr <- function(x, level = 1, ...) {
   cat("Multiscale OME-Zarr object.\n")
-  cat(sprintf("Scale: %d/%d", level, length(x)))
+  cat(sprintf("Scale: %d/%d", level, length(x)), "\n")
   print(head(x[[level]], rep_len(5, length(dim(x[[level]]))), ...))
 }

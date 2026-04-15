cad8c78941001c68bab8fe1871022e6906c24788
Hugo Gruson <git@hugogruson.fr>
2026-04-15 12:01:29 +0200

    Fix schemas

--- data-raw/download-consolidate-schemas.R
+++ data-raw/download-consolidate-schemas.R
@@ -10,17 +10,17 @@ download_consolidate_schemas <- function(ome_version, path) {
   # Fetch references and transform them to local references as jsonvalidate
   # doesn't support remote references (https://github.com/ropensci/jsonvalidate/issues/70)
   schema <- jsonlite::read_json(dest)
-  flat_schema <- unlist(schema, recursive = TRUE)
-  purrr::imap(flat_schema, function(value, name) {
-    if (endsWith(name, "$ref") && is.character(value) && grepl("^https://ngff.openmicroscopy.org/.+/schemas/.+\\.schema$", value)) {
-      ref_url <- value
-      ref_name <- basename(ref_url)
-      ref_dest <- file.path(path, ome_version, ref_name)
-      download.file(ref_url, ref_dest)
-      flat_schema[[name]] <<- ref_name
+  purrr::modify_tree(
+    schema,
+    leaf = function(x) {
+      if (is.character(x) && grepl("^https://ngff.openmicroscopy.org/.+/schemas/.+\\.schema$", x)) {
+        download.file(x, file.path(path, ome_version, basename(x)))
+        return(basename(x))
+      } else {
+        return(x)
+      }
     }
-  })
-  relist(flat_schema, schema) |> 
+  ) |> 
     jsonlite::write_json(dest, auto_unbox = TRUE, pretty = TRUE)
 }
 

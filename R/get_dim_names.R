6c184a9fd2009069b65b62908d28f831c2401f52
Hugo Gruson <git@hugogruson.fr>
2026-04-15 10:17:46 +0200

    Fix get_dim_names for 0.3 and 0.4

--- R/get_dim_names.R
+++ R/get_dim_names.R
@@ -1,11 +1,11 @@
 #' @keywords internal
-.get_dim_names <- function(metadata, ome_version) {
+.get_dim_names <- function(metadata, ome_version) { 
   dim_names <- switch(
     ome_version,
     "0.1" = NULL,
     "0.2" = NULL,
-    "0.3" = metadata$ome$multiscales[[1]]$axes,
-    "0.4" = metadata$ome$multiscales[[1]]$axes |> vapply(function(axis) axis$name, character(1)),
+    "0.3" = metadata$multiscales[[1]]$axes,
+    "0.4" = metadata$multiscales[[1]]$axes |> vapply(function(axis) axis$name, character(1)),
     "0.5" = metadata$ome$multiscales[[1]]$axes |> vapply(function(axis) axis$name, character(1)),
     stop("Unsupported OME version: ", ome_version)
   )

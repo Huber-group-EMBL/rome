43bd11ef94ab0c410aae0a38a265538e15c41611
Hugo Gruson <git@hugogruson.fr>
2026-04-15 16:25:49 +0200

    Remove outdated cache line

--- R/ome_validate.R
+++ R/ome_validate.R
@@ -3,8 +3,6 @@ ome_validate <- function(path, s3_client = NULL) {
   group_attributes <- Rarr::read_zarr_attributes(path, s3_client = s3_client)
   ome_version <- group_attributes$ome$version
 
-  cache_folder <- tools::R_user_dir("rome", which = "cache")
-
   # We cannot download the schemas on the fly because we patch them to use local references 
   # as jsonvalidate doesn't support remote references
   # (https://github.com/ropensci/jsonvalidate/issues/70)

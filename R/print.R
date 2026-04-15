152dc6a7eba56d81d74b09225d84282f9af69203
Hugo Gruson <git@hugogruson.fr>
2026-04-15 10:14:55 +0200

    Print in print method

--- R/print.R
+++ R/print.R
@@ -2,5 +2,5 @@
 print.ome_zarr <- function(x, level = 1, ...) {
   cat("Multiscale OME-Zarr object.\n")
   cat(sprintf("Scale: %d/%d", level, length(x)))
-  head(x[[level]], rep_len(5, length(dim(x[[level]]))), ...)
+  print(head(x[[level]], rep_len(5, length(dim(x[[level]]))), ...))
 }
